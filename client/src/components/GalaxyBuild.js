a42fe1b4dbc15cdcbf8795e6ab943ed01630decf
init.defaultBranch <benlimpic@gmail.com>
2022-06-10 21:08:16 -0700

    less css / jquery, and html

--- client/src/components/GalaxyBuild.js
+++ client/src/components/GalaxyBuild.js
@@ -4,66 +4,16 @@ import $ from 'jquery';
 
 function GalaxyBuild() {
 
-    var body = $("body"),
-        universe = $("#universe"),
-        solarsys = $("#solar-system");
+    var body = $("body")
 
     var init = function() {
-        body.removeClass('view-3D opening').addClass("view-2D").delay(2000).queue(function() {
-        $(this).removeClass('hide-UI').addClass("set-speed");
-        $(this).dequeue();
-        });
+        body.removeClass('view-3D opening').addClass("view-2D").delay(2000)
     };
-
-    $("#toggle-data").click(function(e) {
-        body.toggleClass("data-close");
-        e.preventDefault();
-    });
-
-    $("#toggle-controls").click(function(e) {
-        body.toggleClass("controls-close");
-        e.preventDefault();
-    });
-
-    $("#data a").click(function(e) {
-        var ref = $(this).attr("class");
-        solarsys.removeClass().addClass(ref);
-        $(this).parent().find('a').removeClass('active');
-        $(this).addClass('active');
-        e.preventDefault();
-    });
 
     init();
 
-
     return (
         <div>
-        <div id="navbar">
-        <a id="toggle-data" href="#data"><i className="icon-data"></i></a>
-        <a id="toggle-controls" href="#controls"><i className="icon-controls"></i></a>
-        </div>
-        <div >
-        {/* <div id="data">
-        <a className="sun" title="sun" href="#sunspeed">Sun</a>
-        <a className="mercury" title="mercury" href="#mercuryspeed">Mercury</a>
-        <a className="venus" title="venus" href="#venusspeed">Venus</a>
-        <a className="earth active" title="earth" href="#earthspeed">Earth</a>
-        <a className="mars" title="mars" href="#marsspeed">Mars</a>
-        <a className="jupiter" title="jupiter" href="#jupiterspeed">Jupiter</a>
-        <a className="saturn" title="saturn" href="#saturnspeed">Saturn</a>
-        <a className="uranus" title="uranus" href="#uranusspeed">Uranus</a>
-        <a className="neptune" title="neptune" href="#neptunespeed">Neptune</a>
-        </div> */}
-
-        {/* <div id="controls">
-        <label className="set-view" onClick={(e) => body.toggleClass("view-3D view-2D")}>
-            <input type="checkbox" />
-        </label> */}
-        {/* <label className="set-zoom" onClick={(e) => body.toggleClass("zoom-large zoom-close")
-        }>
-            <input type="checkbox" />
-        </label>  */}
-        </div>
         <div className="galaxy">
         <div id="universe" className="scale-stretched">
         <div id="galaxy">
